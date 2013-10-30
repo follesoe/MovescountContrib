8015257e6652dc932ece69ea304162841bd643be
Jonas Follesø <jonas@follesoe.no>
2013-10-30 23:21:25 +0100

    Added new Controller for Calendar Feed.

--- MovescountContrib.Web/Controllers/HomeController.cs
+++ MovescountContrib.Web/Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-﻿using DDay.iCal.Serialization;
-using DDay.iCal.Serialization.iCalendar;
-using MovescountContrib.Lib;
-using System;
-using System.Configuration;
-using System.Text;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MovescountContrib.Web.Controllers
 {
@@ -13,30 +7,6 @@ namespace MovescountContrib.Web.Controllers
         public ActionResult Index()
         {
             return View();
-        }
-
-        public ActionResult Cal(string email)
-        {
-            if (String.IsNullOrWhiteSpace(email))
-            {
-                throw new Exception("Email missing.");
-            }
-
-            var password = ConfigurationManager.AppSettings[email];
-
-            if (String.IsNullOrWhiteSpace(password))
-            {
-                throw new Exception(string.Format("User {0} does not exist.", email));
-            }
-
-            var iCal = PlannedMoves.getTrainingPlanCal(email, password);
-
-            ISerializationContext ctx = new SerializationContext();
-            ISerializerFactory factory = new SerializerFactory();
-            var serializer = (IStringSerializer)factory.Build(iCal.GetType(), ctx);
-
-            string output = serializer.SerializeToString(iCal);
-            return File(Encoding.UTF8.GetBytes(output), "text/calendar", "Movescount.ical");
         }
     }
 }
