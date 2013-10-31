85cf59b32b36d98122cafdc73edb2a50e9344fa1
Jonas Follesø <jonas@follesoe.no>
2013-10-31 01:08:27 +0100

    Set end-time if the description starts with a time.

--- MovescountContrib.Lib/PlannedMoves.fs
+++ MovescountContrib.Lib/PlannedMoves.fs
@@ -1,5 +1,6 @@
 ﻿namespace MovescountContrib.Lib
 
+open System
 open Newtonsoft.Json
 open System.Text.RegularExpressions
 open MovescountContrib.Lib.DataTypes
@@ -8,6 +9,11 @@ module PlannedMoves =
     open MovescountContrib.Lib.MovescountClient
     open MovescountContrib.Lib.Utils
     open DDay.iCal
+
+    let (|Regex|_|) pattern input =
+        let m = Regex.Match(input, pattern)
+        if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+        else None
 
     let getTrainingPlan email password =
         async {
@@ -24,6 +30,14 @@ module PlannedMoves =
             return JsonConvert.DeserializeObject<TrainingPlan>(jsonString, settings)
         }
 
+    let getStartTime (move:ScheduledMove) =
+        match move.Description with
+        | Regex @"(\d{2}):(\d{2})" [hourStr; minuteStr] ->
+            let hours = Convert.ToDouble(hourStr)
+            let minutes = Convert.ToDouble(minuteStr)
+            Some( (move.Day.AddHours(hours).AddMinutes(minutes)) )
+        | _ -> None
+
     let getTrainingPlanCal email password =
         let plan = (getTrainingPlan email password) |> Async.RunSynchronously
         let iCal = new iCalendar()
@@ -32,13 +46,22 @@ module PlannedMoves =
 
         iCal.AddProperty("X-WR-CALNAME", calName)
         iCal.AddProperty("X-WR-CALDESC", calDescription)
+        iCal.AddTimeZone(TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time")) |> ignore
 
         plan.ScheduledMoves |>
             Seq.iter(fun m ->
                 let event = iCal.Create<Event>()
                 event.Properties.Remove("SEQUENCE") |> ignore
-                event.IsAllDay <- true
-                event.Start <- new iCalDateTime(m.Day.Date)
+
+                match getStartTime m with
+                | Some(date) ->
+                    event.IsAllDay <- false
+                    event.Start <- new iCalDateTime(date)
+                    event.End <- new iCalDateTime((date.AddMinutes((Convert.ToDouble(m.Duration)))))
+                | None ->
+                    event.IsAllDay <- true
+                    event.Start <- new iCalDateTime(m.Day.Date)
+
                 event.Description <- m.Description
                 event.Summary <- getEventTitle m
                 event.UID <- (m.ID.ToString())
